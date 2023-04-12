bb783bb32eeb3f48bf68217b53c2fe588510413f
TearsOprah <qmeshokp@mail.ru>
2023-04-12 20:00:11 +0300

    feat: логика работы фильтра

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,125 @@
-export default function Filter() {
+// import React, { useState } from "react";
+// import './Filter.scss'
+//
+// export default function Filter({ brands }) {
+//   const [selectedBrands, setSelectedBrands] = useState([]);
+//
+//   const handleApplyFilters = () => {
+//     // Вызов функции handleApplyFilters с выбранными брендами
+//     // Можете выполнять необходимую логику с выбранными брендами здесь
+//     console.log("Selected brands: ", selectedBrands);
+//   };
+//
+//   const handleResetFilters = () => {
+//     setSelectedBrands([]);
+//     console.log("Selected brands: ", selectedBrands);
+//   };
+//
+//   const handleBrandClick = (brandId) => {
+//     if (selectedBrands.includes(brandId)) {
+//       // Если бренд уже выбран, удаляем его из массива выбранных брендов
+//       setSelectedBrands(prevSelectedBrands =>
+//         prevSelectedBrands.filter(brand => brand !== brandId)
+//       );
+//     } else {
+//       // Если бренд не выбран, добавляем его в массив выбранных брендов
+//       setSelectedBrands(prevSelectedBrands => [...prevSelectedBrands, brandId]);
+//     }
+//   };
+//
+//   return (
+//     <>
+//       <h3>Brands: </h3>
+//       <ul>
+//         {brands.map((brand, index) => (
+//           <li
+//             key={brand.id}
+//             onClick={() => handleBrandClick(brand.id)}
+//             className={selectedBrands.includes(brand.id) ? "brand_active" : ""}
+//             style={{ cursor: "pointer" }}
+//           >
+//             {brand.title}
+//           </li>
+//         ))}
+//       </ul>
+//
+//       <div>
+//         <button onClick={handleApplyFilters}>Apply</button>
+//         <button onClick={handleResetFilters}>Reset</button>
+//       </div>
+//     </>
+//   );
+// }
+
+import React, { useState, useEffect } from "react";
+
+export default function Filter({ brands }) {
+  const [selectedBrands, setSelectedBrands] = useState([]);
+
+  const handleApplyFilters = () => {
+    // Вызов функции handleApplyFilters с выбранными брендами
+    // Можете выполнять необходимую логику с выбранными брендами здесь
+    console.log("Selected brands: ", selectedBrands);
+  };
+
+  const handleResetFilters = () => {
+    setSelectedBrands([]);
+
+    // Снимаем отметки со всех флажков, устанавливая значение checked в false
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+      checkbox.checked = false;
+    });
+
+    console.log("Selected brands: ", selectedBrands);
+  };
+
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+
+    if (checked) {
+      // Если флажок отмечен, добавляем значение в массив выбранных брендов
+      setSelectedBrands(prevSelectedBrands => [...prevSelectedBrands, value]);
+    } else {
+      // Если флажок снят, удаляем значение из массива выбранных брендов
+      setSelectedBrands(prevSelectedBrands => prevSelectedBrands.filter((brandId) => brandId !== value));
+    }
+  };
+
+  useEffect(() => {
+    // Устанавливаем состояние флажков при изменении состояния selectedBrands
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(checkbox => {
+      if (selectedBrands.includes(checkbox.value)) {
+        checkbox.checked = true;
+      } else {
+        checkbox.checked = false;
+      }
+    });
+  }, [selectedBrands]);
+
   return (
     <>
+      <h3>Brands: </h3>
+      <ul>
+        {brands.map((brand) => (
+          <li key={brand.id}>
+            <label>
+              <input
+                type={"checkbox"}
+                value={brand.id}
+                onChange={handleCheckboxChange}
+              />
+              {brand.title}
+            </label>
+          </li>
+        ))}
+      </ul>
+
+      <div>
+        <button onClick={handleApplyFilters}>Apply</button>
+        <button onClick={handleResetFilters}>Reset</button>
+      </div>
     </>
-  )
+  );
 }
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,20 +1,83 @@
-import products from '../../assets/products.json'
+import productsData from '../../assets/products.json'
+import brandsData from '../../assets/brands.json';
 import ProductCard from "../ProductCard/ProductCard";
+import './ProductList.scss'
+import Filter from "../Filter/Filter";
+import {useEffect, useState} from "react";
 
 
 export default function ProductList() {
 
+  const [products, setProducts] = useState([]);
+  const [brands, setBrands] = useState([]);
+
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const [isFilterApplied, setIsFilterApplied] = useState(false); // Добавляем состояние для флага применения фильтра
+
+  useEffect(() => {
+    // загрузка данных продуктов из productsData
+    setProducts(productsData);
+    // загрузка данных брендов из brandsData
+    setBrands(brandsData);
+  }, []);
+
+
+  // // Обработчик события при изменении состояния чекбокса выбора бренда
+  // const handleBrandChange = (event) => {
+  //   const brandId = event.target.value;
+  //   const isChecked = event.target.checked;
+  //
+  //   // Если чекбокс отмечен, добавляем бренд в список выбранных брендов
+  //   // Если чекбокс снят, удаляем бренд из списка выбранных брендов
+  //   setSelectedBrands(prevSelectedBrands => {
+  //     if (isChecked) {
+  //       return [...prevSelectedBrands, brandId];
+  //     } else {
+  //       return prevSelectedBrands.filter(id => id !== brandId);
+  //     }
+  //   });
+  // };
+
+  // Обработчик события для кнопки "Применить"
+  // const handleApplyFilters = () => {
+  //   // Фильтруем продукты на основе выбранных брендов
+  //   const filteredProducts = products.filter(product => selectedBrands.length === 0 || selectedBrands.includes(product.brand));
+  //   setFilteredProducts(filteredProducts);
+  //   setIsFilterApplied(true)
+  // };
+
+  // Обработчик события для кнопки "Сбросить"
+  const handleResetFilters = () => {
+    setSelectedBrands([]);
+    setFilteredProducts([]);
+    setIsFilterApplied(false)
+  };
+
+
   return (
     <>
       <h2>ProductList</h2>
-      <ul>
-        {products.map(product => (
-          <ProductCard key={product.id}
+
+      <Filter brands={brands}
+
+      />
+
+      <ul className={'products'}>
+        {
+          isFilterApplied ?
+            filteredProducts.map(product => (<ProductCard key={product.id}
                        title={product.title}
                        regular_price={product.regular_price}
                        image={require(`../../assets${product.image}`)}
-                       brand={product.brand}/>
-        ))}
+                       brand={product.brand}/>))
+            : products.map(product => (<ProductCard key={product.id}
+                       title={product.title}
+                       regular_price={product.regular_price}
+                       image={require(`../../assets${product.image}`)}
+                       brand={product.brand}/>))
+        }
       </ul>
     </>
   )
